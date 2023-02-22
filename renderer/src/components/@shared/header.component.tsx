16408b06ee18bd32707ce5405d79f4b2ae198034
lfernando <fernandocostadev98@gmail.com>
2023-02-21 22:42:38 -0300

    chore(components): add shared header

---
+++ renderer/src/components/@shared/header.component.tsx
@@ -0,0 +1,59 @@
+import { Avatar, Flex, IconButton, Text, Tooltip } from '@chakra-ui/react';
+import { CounterClockwiseClockIcon } from '@radix-ui/react-icons';
+
+export const Header = () => {
+  return (
+    <Flex
+      py={2}
+      w='100%'
+      h='4rem'
+      px={'5'}
+      position='sticky'
+      marginBottom={10}
+      zIndex={10}
+      top={0}
+      maxW={'100vw'}
+      alignItems='center'
+      borderBottomWidth={1}
+      justify={'space-between'}
+    >
+      <Text
+        color={'gray.300'}
+        bgClip='text'
+        fontWeight='bold'
+        bgGradient={'linear(to-l, #7928CA, #FF0080)'}
+      >
+        on.focus
+      </Text>
+
+      <Tooltip
+        hasArrow
+        bgColor={'gray.700'}
+        color='white'
+        placement='bottom'
+        label='See profile'
+        size='sm'
+        aria-label='See profile'
+      >
+        <Avatar size='sm' name='Fernando Costa' bgColor='blue.500' />
+      </Tooltip>
+
+      <Tooltip
+        hasArrow
+        placement='left'
+        colorScheme={'gray'}
+        label={'See history'}
+      >
+        <IconButton
+          size='sm'
+          bg={'transparent'}
+          _hover={{
+            bgColor: 'gray.700',
+          }}
+          aria-label='See history'
+          icon={<CounterClockwiseClockIcon />}
+        />
+      </Tooltip>
+    </Flex>
+  );
+};
